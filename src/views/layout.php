1462942d80dcc52acf625157ea2bcde3295994a9
Lpiedagnel <piedagnel.dev@gmail.com>
2023-05-24 16:54:18 +0200

    Add navbar icon on responsive

--- src/views/layout.php
+++ src/views/layout.php
@@ -5,30 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?></title>
     <meta name="description" content="<?= $description ?>">
+    <script src="https://kit.fontawesome.com/ef8b364259.js" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="https://picocss.com/favicon.ico">
     <link rel="canonical" href="https://picocss.com/examples/sign-in/">
-
     <!-- Pico.css -->
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
     <link rel="stylesheet" href="src/css/style.css">
 </head>
 <body>
 <!-- Nav -->
-<nav class="container-fluid">
+<nav class="container-fluid navbar">
     <ul>
         <li><a href="./" class="contrast" onclick="event.preventDefault()"><strong>WorkTogether</strong></a></li>
     </ul>
     <ul>
-        <li><a href="index.php?controller=message&action=feed">Accueil</a></li>
+        <li><a href="index.php?controller=message&action=feed" class="navbar__link--desktop">Accueil</a></li>
+        <li><a href="index.php?controller=message&action=feed" class="navbar__link--mobile"><i class="fa fa-home" aria-hidden="true"></i></a></li>
         <?php if (isset($_SESSION['is_connected'])): ?>
-            <li><a href="index.php?controller=user&action=logout">Déconnexion</a></li>
-            <li><a href="index.php?controller=user&action=update" role="button">Profil</a></li>
+            <li><a href="index.php?controller=user&action=logout" class="navbar__link--desktop">Déconnexion</a></li>
+            <li><a href="index.php?controller=user&action=logout" class="navbar__link--mobile"><i class="fa fa-sign-out" aria-hidden="true"></i></a></li>
+            <li><a href="index.php?controller=user&action=update" role="button" class="navbar__link--desktop">Profil</a></li>
+            <li><a href="index.php?controller=user&action=update" role="button" class="navbar__link--mobile"><i class="fa fa-user" aria-hidden="true"></i></a></li>
             <?php if ($_SESSION['is_admin'] === 1): ?>
-                <li><a href="index.php?controller=user&action=admin" role="button" class="secondary">Administration</a></li>
+                <li><a href="index.php?controller=user&action=admin" role="button" class="secondary navbar__link--desktop">Administration</a></li>
+                <li><a href="index.php?controller=user&action=admin" role="button" class="secondary navbar__link--mobile"><i class="fa fa-cog" aria-hidden="true"></i></a></li>
             <?php endif ?>
         <?php else: ?>
-            <li><a href="index.php">Se connecter</a></li>
-            <li><a href="index.php?controller=user&action=signup" role="button">Inscription</a></li>
+            <li><a href="index.php" class="navbar__link--desktop">Se connecter</a></li>
+            <li><a href="index.php" class="navbar__link--mobile"><i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
+            <li><a href="index.php?controller=user&action=signup" role="button" class="navbar__link--desktop">Inscription</a></li>
+            <li><a href="index.php?controller=user&action=signup" role="button" class="navbar__link--mobile"><i class="fa fa-user-plus" aria-hidden="true"></i></a></li>
        <?php endif ?> 
     </ul>
 </nav>
